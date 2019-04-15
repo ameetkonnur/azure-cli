8822df7ed36821acf18e736746fc480064c1545e
Ameet Konnur <ameetk@live.in>
2019-04-15 15:54:35 +0530

    20190415

--- update-blob.sh
+++ update-blob.sh
@@ -5,7 +5,7 @@ container='<container name>'
 key='<storage account key>'
 pattern='<pattern> for eg .log .jpg'
 mime_type='<MIME Type>'
-az storage blob list --query "[?contains(name,'$pattern')].name" --container-name $container --account-name $account --account-key $key -o tsv >> blobs.txt
+az storage blob list --query "[?contains(name,'$pattern')].name" --num-results "*" --container-name $container --account-name $account --account-key $key -o tsv >> blobs.txt
 for i in `cat blobs.txt`
 do
 az storage blob update --container-name $container --account-name $account --account-key $key --name $i --content-type $mime_type
